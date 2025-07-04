9b4e5187b539843b36787823bf5608b2c4cc4f1d
mrtanonur <onurtan@gmail.com>
2025-07-04 22:04:53 +0300

    feat(constants): Add color and sizes constants

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0x89000000);
+  static const Color accent = Color(0xFF9C27B0);
+  static const Color grey100 = Color(0xffF5F5F5);
+  static const Color grey300 = Color(0xffD9D9D9);
+  static const Color grey500 = Color(0xffABABAB);
+  static const Color grey600 = Color(0xff5C5C5C);
+  static const Color grey700 = Color(0xff4A4A4A);
+  static const Color grey800 = Color(0xff474747);
+  static const Color grey900 = Color(0xff2E2E2E);
+}
